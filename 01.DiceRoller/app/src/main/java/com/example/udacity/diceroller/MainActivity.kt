d0dfcbf1dedce329b2875e031cf642845b419e0d
oliveira-marcio <emaildomso@gmail.com>
2019-07-30 17:58:54 +0100

    feat: Add button listener and display random dice rolled

--- 01.DiceRoller/app/src/main/java/com/example/udacity/diceroller/MainActivity.kt
+++ 01.DiceRoller/app/src/main/java/com/example/udacity/diceroller/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.udacity.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.resultText
+import kotlinx.android.synthetic.main.activity_main.rollButton
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = "$randomInt"
     }
 }
