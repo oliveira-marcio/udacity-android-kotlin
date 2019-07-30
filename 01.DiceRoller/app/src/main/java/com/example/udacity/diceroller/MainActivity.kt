f1f9b59645c2ef8c92b7ea49926c884d9b675b09
oliveira-marcio <emaildomso@gmail.com>
2019-07-30 18:09:48 +0100

    Replace text view by dice images

--- 01.DiceRoller/app/src/main/java/com/example/udacity/diceroller/MainActivity.kt
+++ 01.DiceRoller/app/src/main/java/com/example/udacity/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.udacity.diceroller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.resultText
+import kotlinx.android.synthetic.main.activity_main.diceImage
 import kotlinx.android.synthetic.main.activity_main.rollButton
 import java.util.Random
 
@@ -19,6 +19,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = "$randomInt"
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
