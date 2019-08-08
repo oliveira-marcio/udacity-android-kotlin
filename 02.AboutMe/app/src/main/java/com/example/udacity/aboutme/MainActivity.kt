aa76db9745720449909b2bb44ab027d1f047e983
oliveira-marcio <emaildomso@gmail.com>
2019-08-08 19:02:33 +0100

    feat: Add edit text and done button

--- 02.AboutMe/app/src/main/java/com/example/udacity/aboutme/MainActivity.kt
+++ 02.AboutMe/app/src/main/java/com/example/udacity/aboutme/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.udacity.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        nicknameText.text = nicknameEdit.text
+        view.visibility = View.GONE
+        nicknameEdit.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
