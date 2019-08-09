29bf63fe521ceb19c06c9aa65500f00be68b0780
oliveira-marcio <emaildomso@gmail.com>
2019-08-09 14:13:37 +0100

    refactor: Bind data to text views

--- 02.AboutMe/app/src/main/java/com/example/udacity/aboutme/MainActivity.kt
+++ 02.AboutMe/app/src/main/java/com/example/udacity/aboutme/MainActivity.kt
@@ -12,9 +12,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName = MyName("Márcio Oliveira")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         binding.doneButton.setOnClickListener {
             addNickname(it)
@@ -23,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
